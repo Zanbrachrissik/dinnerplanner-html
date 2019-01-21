d769c8ae82cb6720931083ec713674fe2129ea42
Ke Zhang <4everTomchristmas@gmail.com>
2019-01-21 14:44:45 +0100

    first push
    1.21

---
+++ js/model/index2_JS.js
@@ -0,0 +1,8 @@
+	var numberOfGuests;
+
+	var numberOfGuests = document.getElementById("guestNumber").value;
+	var foodType = document.getElementById("foodType").value;
+
+	this.getSelectedDish = function() {
+		console.log("foodType");
+	}
