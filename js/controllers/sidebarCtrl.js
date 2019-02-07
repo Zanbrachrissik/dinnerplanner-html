8137080d69106f3df676fca83ed4bfdfed1b7da0
Zanbrachrissik <4everTomchristmas@gmail.com>
2019-02-07 17:03:52 +0100

    .

--- js/controllers/sidebarCtrl.js
+++ js/controllers/sidebarCtrl.js
@@ -2,15 +2,10 @@ var SidebarCtrl = function(view, model, navigationDelegate){
 
 	view.guestNumberView.onchange = function () {
 		var GuestNumber=document.getElementById("guestNumber").value;
-		//console.log(GuestNumber);
 		model.setNumberOfGuests(GuestNumber);
 	}
 
 	view.confirmDinnerButton.onclick = function(){
-		// $("#SidebarView").hide();
-		// $("#DishSearchView").hide();
-		// $("#DishDetailsView").hide();
-		// $("#ConfirmPageView").show();
 		navigationDelegate.navigateTo("ConfirmPageView");
 	}
 
